1927e6a0519b6b1d99a902be10edc96967001fb4
vmhowley <vmhowleyh@gmail.com>
2024-10-30 10:27:27 -0400

    last changes

--- traveler-price/src/routes/Home.jsx
+++ traveler-price/src/routes/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   const [from, setFrom] = useState([])
   const [to, setTo] = useState([])
   const [open, setOpen] = useState(null)
-  const [locations, setLocations] = useState()
+  const [locations, setLocations] = useState([{}])
   const [keyword, setKeyword] = useState()
 
   const handleInput = (e) => {
@@ -15,13 +15,14 @@ function Home() {
     console.log(citySearch)
   }
   const handleFromTab = (e) => {
+    document.querySelector('#from').focus()
     setOpen(!open)
     console.log('open')
   }
   const handleFrom = async (e) => {
     setKeyword(e.target.value)
     
-    const url = `https://reply-buy-rid-duke.trycloudflare.com/api/citysearch?keyword=${e.target.value}`
+    const url = `https://logged-gmbh-entering-totally.trycloudflare.com/api/citysearch?keyword=${e.target.value}`
     try {
       const response = await fetch(url)
       if (!response.ok) {
@@ -62,7 +63,9 @@ function Home() {
   }
 
   const handleSelection = (e) => {
-    alert(e.name)
+    console.log(e)
+    setLocations(e)
+    setOpen(!open)
   }
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -79,34 +82,33 @@ function Home() {
           <div className=' p-2 rounded-full w-full'>Multicity</div>
         </div>
         <form
-          className='bg-white p-4 shadow rounded-xl grid gap-6 static'
-          action=''
+          className='bg-white p-4 shadow rounded-xl grid sm:flex gap-6 sm:gap-8 static'
         >
           <div
-            onClick={handleFromTab}
-            className={`grid w-full relative ${
-              open ? 'fixed inset-0 z-50' : ''
-            }`}
+            className={`grid w-full relative `}
           >
             <div className='w-full relative'>
-              <div className='absolute '>
-                <h1>Delhi DEL</h1>
-              </div>
+                <button type='button' onClick={handleFromTab} className={`w-full relative  ${open? 'z-0' : 'z-40'}   rounded-xl  border h-14 p-2 placeholder-gray-950 font-semibold text-left ps-6`}>
+                  {locations.name }
+                </button>
               <input
-                className={`w-full z-50  border h-14 p-2 placeholder-gray-950 font-semibold ps-8 ${
-                  open ? 'fixed inset-0' : 'rounded-xl  '
+              
+                className={`w-full  border h-14 p-2 placeholder-gray-950 font-semibold ps-8  ${
+                  open ? 'fixed inset-0 sm:absolute rounded-xl    ' : 'rounded-xl  absolute left-0  '
                 } `}
                 type='text'
                 id='from'
                 name='from'
-                placeholder='Leaving from'
+                autoComplete="off"
+                
                 onChange={handleFrom}
+                
               />
             </div>
             <section
               id='from_country'
               className={`border  bg-white top-14 ${
-                open ? 'fixed inset-0 z-50' : 'hidden '
+                open ? 'fixed inset-0 sm:absolute sm:w-96 sm:h-96 z-50 ' : 'hidden '
               }`}
               name='from_country'
               onChange={handleInput}
@@ -116,7 +118,7 @@ function Home() {
                   ? from.map((location) => (
                       <div key={location.id}>
                         <li
-                          onClick={(e) => {handleSelection(location)}}
+                          onClick={() => {handleSelection(location)}}
                           key={location.id}
                           value={location.name}
                           className='capitalize cursor-pointer rounded-xl p-2'
@@ -129,7 +131,7 @@ function Home() {
                   : ''}
               </ul>
             </section>
-          <div className='absolute right-6 top-12 z-10'>
+          <div className='absolute right-6 top-12 z-30 sm:-right-9 sm:top-2'>
             <img
               className='w-max rounded-full p-2 border bg-white'
               src={ArrowsDownUpIcon}
@@ -137,33 +139,47 @@ function Home() {
               />
           </div>
               </div>
-          <div className='grid'>
-            <input
-              className='border rounded-xl h-14 p-2 placeholder-gray-950 font-semibold ps-8'
-              type='text'
-              id='to'
-              name='to'
-              placeholder='Going to'
-              onChange={handleFrom}
-            />
-            <ul id='from_country' name='from_country' onChange={handleInput}>
-              {!to.code
-                ? to.map((location) => (
-                    <div
-                      onClick={() => {
-                        console.log(location.name)
-                      }}
-                      key={location.id}
-                      value={location.name}
-                      className='capitalize cursor-pointer bg-white'
-                    >
-                      {location.address.cityName.toLowerCase()} (
-                      {location.name.toLowerCase()} - {location.iataCode})
-                    </div>
-                  ))
-                : ''}
-            </ul>
-          </div>
+              <div className='w-full relative'>
+                <button type='button' onClick={handleFromTab} className={`w-full relative  ${open? '' : 'z-40'}   rounded-xl  border h-14 p-2 placeholder-gray-950 font-semibold text-left ps-6`}>
+                  {locations.name }
+                </button>
+              <input
+                className={`w-full  border h-14 p-2 placeholder-gray-950 font-semibold ps-8 bg-black  ${
+                  open ? 'fixed inset-0  sm:absolute rounded-xl hidden    ' : 'rounded-xl  absolute left-0 z-0 '
+                } `}
+                type='text'
+                autoComplete="off"
+                id='to'
+                name='to'
+                onChange={handleFrom}
+              />
+            </div>
+            <section
+              id='from_country'
+              className={`border  bg-white top-14 ${
+                open ? 'fixed inset-0 sm:absolute sm:w-96 sm:h-96 sm:z-50 ' : 'hidden '
+              }`}
+              name='from_country'
+              onChange={handleInput}
+            >
+              <ul>
+                {!to.code
+                  ? to.map((location) => (
+                      <div key={location.id}>
+                        <li
+                          onClick={() => {handleSelection(location)}}
+                          key={location.id}
+                          value={location.name}
+                          className='capitalize cursor-pointer rounded-xl p-2'
+                        >
+                          {location.address.cityName.toLowerCase()} (
+                          {location.name.toLowerCase()} - {location.iataCode})
+                        </li>
+                      </div>
+                    ))
+                  : ''}
+              </ul>
+            </section>
           <div></div>
           <button type='submit'></button>
         </form>
