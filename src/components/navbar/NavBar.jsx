67fbfbca0b2dd728f545f33c906ca07ffd25da62
vmhowley <vmhowleyh@gmail.com>
2024-11-04 09:24:02 -0400

    last

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import HomeIcon from '../../assets/icons/home.svg'
 import BookingIcon from '../../assets/icons/Bookings.svg'
 import OfferIcon from '../../assets/icons/Offer2.svg'
-import InboxIcon from '../../assets/icons/Inbox.svg'
-import UserIcon from '../../assets/icons/User.svg'
+import InboxIcon from '../../assets/icons/inbox.svg'
+import UserIcon from '../../assets/icons/user.svg'
 import { Link } from 'react-router-dom'
 function NavBar() {
   return (
